bfaa8e55671c8158ae6eb3ddf9a4f4e0ad2ba8f8
rjewing <ewing.rj@gmail.com>
2017-05-11 06:54:23 -0700

    Moved csv order export to admin action.
    Added action to download order invoices as zip file

--- ffcsa/core/admin.py
+++ ffcsa/core/admin.py
@@ -1,69 +1,99 @@
 from __future__ import unicode_literals
 
 import csv
+import tempfile
+import zipfile
 
 from django.contrib.auth import get_user_model
 from django.http import HttpResponse
+from django.template.loader import get_template
 from django.urls import reverse
 
-from cartridge.shop.models import Category, Product, Order, ProductVariation
+from cartridge.shop.models import Category, Product, Order
 from django.contrib import admin
 
 from cartridge.shop import admin as base
 
 from mezzanine.generic.models import ThreadedComment
+from weasyprint import HTML
 
 from ffcsa.core.availability import inform_user_product_unavailable
-from ffcsa.core.forms import CategoryAdminForm, OrderExportForm
+from ffcsa.core.forms import CategoryAdminForm
+
+
+def export_as_csv(modeladmin, request, queryset):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="ffcsa_order_export.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['Order Date', 'First Name', 'Last Name', 'Item', 'Description', 'Category',
+                     'Unit Price', 'Quantity', 'Total Price'])
+
+    for order in queryset:
+        user = get_user_model().objects.get(id=order.user_id)
+        row_base = [order.time, user.first_name, user.last_name]
+
+        for item in order.items.all():
+            p = Product.objects.filter(sku=item.sku).first()
+
+            # category = p.categories.exclude(slug='weekly-box').first()
+            # sub_category = None
+
+            # if category.parent:
+            #     sub_category = category
+            #     category = sub_category.parent
+
+            row = row_base.copy()
+            row.append(item.description)
+            row.append(p.content)
+            row.append(",".join([c.titles for c in p.categories.exclude(slug='weekly-box')]))
+            # row.append(sub_category.title)
+            row.append(item.unit_price)
+            row.append(item.quantity)
+            row.append(item.total_price)
+
+            writer.writerow(row)
+
+    return response
+
+
+export_as_csv.short_description = "Export As CSV"
+
+
+def download_invoices(self, request, queryset):
+    invoices = {}
+
+    for order in queryset:
+        context = {"order": order}
+        context.update(order.details_as_dict())
+        html = get_template("shop/order_invoice_pdf.html").render(context)
+
+        invoice = tempfile.SpooledTemporaryFile()
+        HTML(string=html).write_pdf(invoice)
+        invoices[order.id] = invoice
+        # Reset file pointer
+        invoice.seek(0)
+
+    with tempfile.SpooledTemporaryFile() as tmp:
+        with zipfile.ZipFile(tmp, 'w', zipfile.ZIP_DEFLATED) as archive:
+            for order_id, invoice in invoices.items():
+                archive.writestr("ffcsa_order_{}.pdf".format(order_id), invoice.read())
+                invoice.close()
+
+        # Reset file pointer
+        tmp.seek(0)
+
+        # Write file data to response
+        response = HttpResponse(tmp.read(), content_type='application/x-zip-compressed')
+        response['Content-Disposition'] = 'attachment; filename="ffcsa_order_invoices.zip"'
+        return response
+
+
+download_invoices.short_description = "Download Invoices"
 
 
 class MyOrderAdmin(base.OrderAdmin):
-    change_list_template = 'admin/ffcsa_core/extras/orders_change_list.html'
-
-    def changelist_view(self, request, extra_context=None):
-        export_form = OrderExportForm(request.POST or None)
-        if request.method == 'POST' and request.POST.get('export_orders'):
-            if export_form.is_valid():
-                orders = export_form.get_orders()
-
-                response = HttpResponse(content_type='text/csv')
-                response['Content-Disposition'] = 'attachment; filename="ffcsa_order_export.csv"'
-                writer = csv.writer(response)
-                # writer.writerow(['Order Date', 'First Name', 'Last Name', 'Item', 'Description', 'Category', 'Sub Category',
-                #                  'Unit Price', 'Quantity', 'Total Price'])
-                writer.writerow(['Order Date', 'First Name', 'Last Name', 'Item', 'Description', 'Category',
-                                 'Unit Price', 'Quantity', 'Total Price'])
-
-                for order in orders:
-                    user = get_user_model().objects.get(id=order.user_id)
-                    row_base = [order.time, user.first_name, user.last_name]
-
-                    for item in order.items.all():
-                        p = Product.objects.filter(sku=item.sku).first()
-
-                        # category = p.categories.exclude(slug='weekly-box').first()
-                        # sub_category = None
-
-                        # if category.parent:
-                        #     sub_category = category
-                        #     category = sub_category.parent
-
-                        row = row_base.copy()
-                        row.append(item.description)
-                        row.append(p.content)
-                        row.append(",".join([c.titles for c in p.categories.exclude(slug='weekly-box')]))
-                        # row.append(sub_category.title)
-                        row.append(item.unit_price)
-                        row.append(item.quantity)
-                        row.append(item.total_price)
-
-                        writer.writerow(row)
-
-                return response
-
-        context = {"export_form": export_form}
-        context.update(extra_context or {})
-        return super().changelist_view(request, context)
+    actions = [export_as_csv, download_invoices]
 
 
 class MyCategoryAdmin(base.CategoryAdmin):
--- ffcsa/core/forms.py
+++ ffcsa/core/forms.py
@@ -25,11 +25,3 @@ class CartDinnerForm(forms.Form):
         if 'attending_dinner' in self.changed_data:
             self._cart.attending_dinner = self.cleaned_data['attending_dinner']
             self._cart.save()
-
-
-class OrderExportForm(forms.Form):
-    from_date = forms.DateField(input_formats=['%m-%d-%Y'], widget=forms.TextInput(attrs={'placeholder': '04-01-2017'}))
-    to_date = forms.DateField(input_formats=['%m-%d-%Y'], widget=forms.TextInput(attrs={'placeholder': '04-31-2017'}))
-
-    def get_orders(self):
-        return Order.objects.filter(time__gte=self.cleaned_data['from_date'], time__lte=self.cleaned_data['to_date'])
