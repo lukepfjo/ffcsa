707e447a8213721618ac7102e507b94648da778c
RJ Ewing <rj@rjewing.com>
2019-11-09 07:10:21 -0800

    expose ROLLBAR in template settings

---
+++ ffcsa/core/defaults.py
@@ -0,0 +1,20 @@
+from __future__ import unicode_literals
+
+from django.utils.translation import ugettext_lazy as _
+from mezzanine.conf import register_setting
+
+####################################################################
+#  This first set of settings already exists in Mezzanine but can  #
+#  be overridden or appended to here .        #
+####################################################################
+
+# Append the ffcsa core settings used in templates to the list of settings
+# accessible in templates.
+register_setting(
+    name="TEMPLATE_ACCESSIBLE_SETTINGS",
+    description=_("Sequence of setting names available within templates."),
+    editable=False,
+    default=("ROLLBAR",),
+    append=True,
+)
+
