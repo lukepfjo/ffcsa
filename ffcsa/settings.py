290bf21ebf93240fc593aa24242d1a88bc9c1b67
RJ Ewing <rj@rjewing.com>
2020-04-10 05:40:26 -0700

    generate deliveries csv that is formatted for OptimoRoute

--- ffcsa/settings.py
+++ ffcsa/settings.py
@@ -61,6 +61,15 @@ DROP_SITE_COLORS = {
 
 DROP_SITE_ORDER = ['LCFM', 'Banzhaf', 'Home Delivery', 'W 11th', 'Farm', 'Woodstock', 'St Johns',
                    'PSU', 'Hollywood', ]
+STANDING_DELIVERIES = [
+    # ['Address', 'Name', 'Phone', 'Notes', 'duration', 'tw start', 'tw end', 'Boxes', 'dairy', 'meat', 'flowers']
+    ['669 Greenwood St, Junction City, OR 97448', 'Post Office', '', '', '5'],
+    ['3843 NW Arrowood Circle, Corvallis, OR 97330', 'Banzhaf Dropsite', '', '', '15', '7:00', '16:00'],
+    ['1007 SE 3rd St, Corvallis, OR 97333', '1st Alt South', '', '', '15'],
+    ['2855 NW Grant Ave, Corvallis, OR 97330', '1st Alt North', '', '', '15'],
+    ['922 NW Circle Blvd, Corvallis, OR 97330', 'Market of Choice', '', '', '15'],
+    ['1122 W 11th Avenue, Eugene, OR 97402', 'W 11th Dropsite', '', '', '20', '4:00', '9:00']
+]
 
 # SignRequest settings
 SIGN_REQUEST_SUBDOMAIN = 'ffcsa'
